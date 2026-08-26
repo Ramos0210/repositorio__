a07c63c12e35f3c2b8b01bed3d2ba9000f3ca8db
Ramos0210 <aramos27@ucatolica.edu.co>
2026-08-26 01:38:26 +0000

    Agregar endpoint de eventos

--- main.py
+++ main.py
@@ -3,3 +3,7 @@ app = FastAPI()
 @app.get("/")
 def home():
     return {"mensaje": "Mi API esta funcionando"}
+
+@app.get("/eventos")
+def listar_eventos():
+    return{"eventos": ["CONNITI 2024", "Taller React", "Charla AI"]}
